50d8944be26a417171c39b7a92840fb1414e5404
mmsesay <maej_01@hotmail.com>
2022-01-17 20:23:26 +0000

    Add: method to update post counter Add: method to return most recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def most_recent_comments
+    comments.order('created_at DESC').limit(5)
+  end
 end
