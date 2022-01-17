3bccd0c6b662ac8ef081ee134694c2e5e12b40af
mmsesay <maej_01@hotmail.com>
2022-01-17 19:41:03 +0000

    Update database migrations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,2 @@
 class Comment < ApplicationRecord
-  belongs_to :author, foreign_key: 'author_id'
-  belongs_to :post, foreign_key: 'post_id'
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,2 @@
 class Post < ApplicationRecord
-  belongs_to :author
-  has_many :likes_counter, foreign_key: 'post_id'
-  has_many :comment_counter, foreign_key: 'post_id'
 end
