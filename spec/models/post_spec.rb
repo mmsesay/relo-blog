05e42a792305cfed3f83f8522683cbb5eb8c5780
mmsesay <maej_01@hotmail.com>
2022-01-21 10:06:51 +0000

    Update the tests for user and post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,48 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  user = User.new(name: 'John', bio: 'I am a content creator', photo: '', posts_counter: 0)
-  post = Post.new(title: 'Post 1', text: 'Text...', comments_counter: 0, likes_counter: 0, author_id: user.id)
+  before(:each) { 
+    @user = User.new(name: 'John', bio: 'I am a content creator', photo: '', posts_counter: 0)
+    @post = Post.new(title: 'Post 1', text: 'Text...', comments_counter: 0, likes_counter: 0, author_id: @user.id)
+    10.times { |i| Comment.new(text: "Comment #{i}", author_id: @user.id, post_id: @post.id) }
 
-  before(:each) { post.save }
+    @comments = Comment.where(post_id: @post.id).where(author_id: @user.id).all
+  }
 
   describe 'user validation tests' do
     it 'validates the presence of the title' do
-      post.title = nil
-      expect(post).to_not be_valid
+      @post.title = nil
+      expect(@post).to_not be_valid
     end
 
     it 'validates the presence of the comments_counter' do
-      post.comments_counter = nil
-      expect(post).to_not be_valid
+      @post.comments_counter = nil
+      expect(@post).to_not be_valid
     end
 
     it 'validates the numericality of the comments_counter' do
-      post.comments_counter = 'comment'
-      expect(post).to_not be_valid
+      @post.comments_counter = 'comment'
+      expect(@post).to_not be_valid
     end
 
     it 'validates the presence of the likes_counter' do
-      post.likes_counter = nil
-      expect(post).to_not be_valid
+      @post.likes_counter = nil
+      expect(@post).to_not be_valid
     end
 
     it 'validates the numericality of the likes_counter' do
-      post.likes_counter = '1'
-      expect(post).to_not be_valid
+      @post.likes_counter = '1'
+      expect(@post).to_not be_valid
     end
 
     it 'validates the presence of the author_id' do
-      post.author_id = nil
-      expect(post).to_not be_valid
+      @post.author_id = nil
+      expect(@post).to_not be_valid
     end
   end
 
   describe 'post model methods tests' do
-    before(:each) { 5.times { |i| Comment.new(text: "Comment #{i}", author_id:user.id, post_id: post.id) } }
-    
-    it 'returns the last 3 comments' do
-      expect(post.most_recent_comments).to eq(Comment.order(created_at: :desc).limit(3))
+    it 'returns the last 5 comments' do
+      expect(@post.most_recent_comments).to eq(@comments.order('created_at DESC').limit(5))
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,46 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  user = User.new(name: 'John', bio: 'I am a content creator', photo: '', posts_counter: 0)
+  before(:each) { 
+    @user = User.new(name: 'John', bio: 'I am a content creator', photo: '', posts_counter: 0)
+    5.times do |i|
+      Post.new(title: "Post #{i}", text: "Post #{i} text", comments_counter: 0, likes_counter: 0, author_id: @user.id)
+    end
 
-  before(:each) { user.save }
+    @posts = Post.where(author_id: @user.id).all
+  }
 
   describe 'user validation tests' do
     it 'validates the presence of the name' do
-      user.name = nil
-      expect(user).to_not be_valid
+      @user.name = nil
+      expect(@user).to_not be_valid
     end
 
     it 'validates the presence of the posts_counter' do
-      user.posts_counter = nil
-      expect(user).to_not be_valid
+      @user.posts_counter = nil
+      expect(@user).to_not be_valid
     end
 
     it 'validates the numericality of the posts_counter' do
-      user.posts_counter = 'zero'
-      expect(user).to_not be_valid
+      @user.posts_counter = 'zero'
+      expect(@user).to_not be_valid
     end
 
     it 'validates the posts_counter is greater_than_zero' do
-      user.posts_counter = 5
-      expect(user.posts_counter).to be > 0
+      @user.posts_counter = 5
+      expect(@user.posts_counter).to be > 0
     end
 
     it 'validates the posts_counter is greater_equal_to_zero' do
-      user.posts_counter = 0
-      expect(user.posts_counter).to eq(0)
+      @user.posts_counter = 0
+      expect(@user.posts_counter).to eq(0)
     end
   end
 
-  # describe 'user model methods tests' do
-  #   before(:each) do
-  #     5.times do |i|
-  #       Post.new(title: "Post #{i}", text: "Post #{i} text", comments_counter: 0, likes_counter: 0, author_id: user.id)
-  #     end
-  #   end
-
-  #   it 'returns the most recent post limit to 3 posts' do
-  #     expect(user.most_recent_posts).to eq(Post.order(created_at: :desc).limit(3))
-  #   end
-  # end
+  describe 'user model methods tests' do
+    it 'returns the most recent post limit to 3 posts' do
+      expect(@user.most_recent_posts).to eq(@posts.order('created_at DESC').limit(3))
+    end
+  end
 end
